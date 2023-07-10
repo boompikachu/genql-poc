ca1082383decbbd475dc31a58dd6efcf8b7c5bdf
boompikachu <37561295+boompikachu@users.noreply.github.com>
2023-07-10 14:12:51 +0700

    feat: implement genql with urql

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,53 @@
 import { Inter } from "next/font/google";
+import {
+  OperationContext,
+  RequestPolicy,
+  cacheExchange,
+  fetchExchange,
+  useQuery,
+} from "urql";
+import Image from "next/image";
 
 import {
-  createClient,
-  enumPokemonEnum,
+  QueryGenqlSelection,
+  QueryResult,
   generateQueryOp,
 } from "../../generated/";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+
+import { withUrqlClient } from "next-urql";
 
 const inter = Inter({ subsets: ["latin"] });
 
-// const client = createClient({
-//   url: "<http://your-api>",
-//   headers: {
-//     Authorization: "Bearer xxx",
-//   },
-// });
+export function useTypedQuery<Query extends QueryGenqlSelection>(opts: {
+  query: Query & { __name?: string };
+  pause?: boolean;
+  requestPolicy?: RequestPolicy;
+  context?: Partial<OperationContext>;
+}) {
+  const { query, variables } = generateQueryOp(opts.query);
+  return useQuery<QueryResult<Query>>({
+    ...opts,
+    query,
+    variables,
+  });
+}
 
-// await client.query({
-//   repository: { __args: { name: "genql", owner: "remorses" }, name: true },
-// });
-
-export default function Home() {
+function Home() {
   const [pokedexEntry, setPokedexEntry] = useState(0);
-  const [pokdexEntryInput, setPokdexEntryInput] = useState("0");
+  const [input, setInput] = useState("0");
 
-  const [name, setName] = useState("");
+  const [pokemon] = useTypedQuery({
+    query: {
+      getPokemonByDexNumber: {
+        __args: { number: pokedexEntry },
+        color: true,
+        sprite: true,
+        species: true,
+        types: { name: true },
+      },
+    },
+  });
 
   const updatePokedexEntry = (newPokdexEntry: number) => {
     if (newPokdexEntry > 0) {
@@ -32,31 +55,7 @@ export default function Home() {
     }
   };
 
-  useEffect(() => {
-    const asdf = generateQueryOp({
-      getPokemonByDexNumber: {
-        __args: { number: pokedexEntry },
-        color: true,
-        species: true,
-      },
-    });
-    fetch("https://graphqlpokemon.favware.tech/v7", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        query: asdf.query,
-        variables: asdf.variables,
-      }),
-    })
-      .then((e) => {
-        return e.text();
-      })
-      .then((res) => {
-        setName(res);
-      });
-  }, [pokedexEntry]);
+  const pokemonSprite = pokemon.data?.getPokemonByDexNumber.sprite;
 
   return (
     <main className={`p-2 flex flex-col items-center gap-y-4 ${inter}`}>
@@ -65,24 +64,48 @@ export default function Home() {
         <input
           type="text"
           className="border"
-          value={pokdexEntryInput}
+          value={input}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              updatePokedexEntry(parseInt(input));
+            }
+          }}
           onChange={(e) => {
             const value = e.currentTarget.value;
             if (/^[0-9]{0,4}$/.test(value)) {
-              setPokdexEntryInput(value);
+              setInput(value);
             }
           }}
         />
         <button
           onClick={() => {
-            updatePokedexEntry(parseInt(pokdexEntryInput));
+            updatePokedexEntry(parseInt(input));
           }}
         >
           Search
         </button>
       </div>
 
-      <div>{name}</div>
+      {pokemonSprite && (
+        <Image
+          src={pokemonSprite}
+          alt=""
+          width={100}
+          height={100}
+          className="w-[100px] h-[100px] object-contain"
+        />
+      )}
+      <div>{pokemon.data?.getPokemonByDexNumber.species}</div>
+      <div>
+        {pokemon.data?.getPokemonByDexNumber.types
+          .map((type) => type.name)
+          .join(", ")}
+      </div>
     </main>
   );
 }
+
+export default withUrqlClient((ssrExchange) => ({
+  url: "https://graphqlpokemon.favware.tech/v7",
+  exchanges: [cacheExchange, ssrExchange, fetchExchange],
+}))(Home);
